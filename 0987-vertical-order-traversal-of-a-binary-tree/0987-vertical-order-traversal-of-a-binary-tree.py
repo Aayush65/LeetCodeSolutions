a072c68f592171cdc4a5c131ccbb9bedc486072b
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-01-24 00:28:45 +0530

    Time: 64 ms (28.48%), Space: 14.1 MB (66.47%) - LeetHub

--- 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
@@ -10,22 +10,42 @@ class Solution:
         minX = 0
         maxX = 0
 
-        def traversal(node: TreeNode, x: int, y: int) -> None:
-            nonlocal minX
-            nonlocal maxX
-            minX = min(minX, x)
-            maxX = max(maxX, x)
+        # DFS implementation
+        # def traversal(node: TreeNode, x: int, y: int) -> None:
+        #     nonlocal minX
+        #     nonlocal maxX
+        #     minX = min(minX, x)
+        #     maxX = max(maxX, x)
 
-            if x in xAxis:
-                xAxis[x].append([y, node.val])
-            else:
-                xAxis[x] = [[y, node.val]]
-            if node.left:
-                traversal(node.left, x - 1, y + 1)
-            if node.right:
-                traversal(node.right, x + 1, y + 1)
+        #     if x in xAxis:
+        #         xAxis[x].append([y, node.val])
+        #     else:
+        #         xAxis[x] = [[y, node.val]]
+        #     if node.left:
+        #         traversal(node.left, x - 1, y + 1)
+        #     if node.right:
+        #         traversal(node.right, x + 1, y + 1)
+        # traversal(root, 0, 0)
 
-        traversal(root, 0, 0)
+        # BFS implenmentation
+        q = [[root, 0]]
+        y = 0
+        while q:
+            newQ = []
+            for i, x in q:
+                minX = min(minX, x)
+                maxX = max(maxX, x)
+                if x in xAxis:
+                    xAxis[x].append([y, i.val])
+                else:
+                    xAxis[x] = [[y, i.val]]
+                if i.left:
+                    newQ.append([i.left, x - 1])
+                if i.right:
+                    newQ.append([i.right, x + 1])
+            q = newQ
+            y += 1
+
         vlot = []
         for i in range(minX, maxX + 1):
             level = []
