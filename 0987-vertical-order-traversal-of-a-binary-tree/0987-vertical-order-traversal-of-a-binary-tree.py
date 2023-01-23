417196fb240e363928825c9b839427b59d006ab3
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-01-23 23:31:12 +0530

    Time: 44 ms (47.18%), Space: 14.3 MB (24.35%) - LeetHub

---
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.py
@@ -0,0 +1,41 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def verticalTraversal(self, root: Optional[TreeNode]) -> List[List[int]]:
+        xAxis = {}
+        minX = 0
+        maxX = 0
+
+        def traversal(node: TreeNode, x: int, y: int) -> None:
+            nonlocal minX
+            nonlocal maxX
+            minX = min(minX, x)
+            maxX = max(maxX, x)
+
+            if x in xAxis:
+                last = node.val
+                if xAxis[x][-1][0] == y:
+                    last = max(xAxis[x][-1][1], node.val)
+                    first = min(xAxis[x][-1][1], node.val) 
+                    xAxis[x][-1][1] = first
+                xAxis[x].append([y, last])
+            else:
+                xAxis[x] = [[y, node.val]]
+            if node.left:
+                traversal(node.left, x - 1, y + 1)
+            if node.right:
+                traversal(node.right, x + 1, y + 1)
+
+        traversal(root, 0, 0)
+        vlot = []
+        for i in range(minX, maxX + 1):
+            level = []
+            xAxis[i].sort()
+            for j in xAxis[i]:
+                level.append(j[1])
+            vlot.append(level)
+        return vlot
