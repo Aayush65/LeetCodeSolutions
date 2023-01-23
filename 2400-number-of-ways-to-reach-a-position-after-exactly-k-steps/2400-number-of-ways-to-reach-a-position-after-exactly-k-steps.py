2489cb977d07e702e761e650592e9e79d09b9c4a
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-01-23 22:52:00 +0530

    Time: 5302 ms (13.14%), Space: 757.5 MB (5.08%) - LeetHub

---
+++ 2400-number-of-ways-to-reach-a-position-after-exactly-k-steps/2400-number-of-ways-to-reach-a-position-after-exactly-k-steps.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numberOfWays(self, startPos: int, endPos: int, k: int) -> int:
+        memo = {}
+
+        def dp(pos: int, steps: int) -> int:
+            if (pos, steps) in memo:
+                return memo[(pos, steps)]
+            if k == steps:
+                if pos == endPos and k == steps:
+                    return 1
+                return 0
+            res = dp(pos - 1, steps + 1)
+            res += dp(pos + 1, steps + 1)
+            memo[(pos, steps)] = res
+            return res
+            
+        return dp(startPos, 0) % 1000000007
