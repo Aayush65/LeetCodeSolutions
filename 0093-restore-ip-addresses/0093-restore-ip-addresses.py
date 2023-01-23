bea1c666fd722acb2adc7d7144873d78b2ac9a9a
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-01-23 22:43:47 +0530

    Time: 34 ms (82.48%), Space: 14 MB (28.27%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -1,22 +1,18 @@
 class Solution:
-    def restoreIpAddresses(self, s: str) -> List[str]:
+    def restoreIpAddresses(self, s: str) -> list[str]:
         allIps = []
 
         def dfs(index: int, ip: list[int], dots) -> int:
-            if dots == 0:
-                return
             if index == len(s):
-                if dots == 1:
-                    allIps.append(''.join(ip))
+                if dots == 4:
+                    allIps.append(''.join(ip[:-1]))
                 return
-            if ip:
-                ip.append('.')
+            if dots > 3:
+                return
             if s[index] == '0':
                 ip.append('0')
-                dfs(index + 1, ip, dots - 1)
+                dfs(index + 1, ip + ['.'], dots + 1)
                 ip.pop()
-                if ip:
-                    ip.pop()
                 return
             currNum = ''
             for i in range(index, min(len(s), index + 3)):
@@ -24,10 +20,8 @@ class Solution:
                 if int(currNum) > 255:
                     break
                 ip.append(currNum)
-                dfs(i + 1, ip, dots - 1)
-                ip.pop()
-            if ip:
+                dfs(i + 1, ip + ['.'], dots + 1)
                 ip.pop()
 
-        dfs(0, [], 5)
+        dfs(0, [], 0)
         return allIps
