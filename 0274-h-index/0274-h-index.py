d6363fe68a57cc2bec029396dc0f31d25ef10c07
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-03-14 23:22:27 +0530

    Time: 38 ms (73.42%), Space: 14.3 MB (23.32%) - LeetHub

---
+++ 0274-h-index/0274-h-index.py
@@ -0,0 +1,17 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort()
+        n = len(citations)
+        def check(val: int) -> bool:
+            i = bisect_left(citations, val)
+            return n - i >= val
+        
+        lo = 0
+        hi = n
+        while lo < hi:
+            mid = (lo + hi + 1) // 2
+            if check(mid):
+                lo = mid
+            else:
+                hi = mid - 1
+        return lo
