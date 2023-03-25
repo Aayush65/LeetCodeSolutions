4ef982597b6fb1baaae8fb1f9066422f7d4b138b
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-03-25 12:05:22 +0530

    Time: 83 ms (91.74%), Space: 14.3 MB (51.33%) - LeetHub

--- 0676-implement-magic-dictionary/0676-implement-magic-dictionary.py
+++ 0676-implement-magic-dictionary/0676-implement-magic-dictionary.py
@@ -1,27 +1,26 @@
 class MagicDictionary:
 
     def __init__(self):
-        self.dictionary = set()
+        self.dictionary = defaultdict(list)
         
-
     def buildDict(self, dictionary: List[str]) -> None:
         for i in dictionary:
-            self.dictionary.add(i)
+            self.dictionary[len(i)].append(i)
         
     def search(self, searchWord: str) -> bool:
-        temp = list(searchWord)
-        for i in range(len(temp)):
-            for j in range(26):
-                ch = chr(ord('a') + j)
-                if ch == searchWord[i]:
-                    continue
-                temp[i] = ch
-                if ''.join(temp) in self.dictionary:
-                    return True
-            temp[i] = searchWord[i]
+        for i in self.dictionary[len(searchWord)]:
+            if self.check(i, searchWord):
+                return True
         return False
         
-
+    def check(self, s1, s2):
+        flag = 2
+        for i in range(len(s1)):
+            if s1[i] != s2[i]:
+                flag -= 1
+            if flag < 1:
+                return False
+        return flag == 1
 
 # Your MagicDictionary object will be instantiated and called as such:
 # obj = MagicDictionary()
