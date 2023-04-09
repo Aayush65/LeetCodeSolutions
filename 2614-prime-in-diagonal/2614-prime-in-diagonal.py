b85dd1b575fb8ace65b99e2d422dd361e90c1259
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-04-09 10:43:24 +0530

    Time: 5194 ms (6.67%), Space: 326.7 MB (6.67%) - LeetHub

--- 2614-prime-in-diagonal/2614-prime-in-diagonal.py
+++ 2614-prime-in-diagonal/2614-prime-in-diagonal.py
@@ -4,22 +4,18 @@ class Solution:
         for i in range(len(nums)):
             diag.add(nums[i][i])
             diag.add(nums[i][len(nums) - i - 1])
-                
-        def isPrime(n):
-            if n == 2 or n == 3: return True
-            if n < 2 or n%2 == 0: return False
-            if n < 9: return True
-            if n%3 == 0: return False
-            r = int(n**0.5)
-            f = 5
-            while f <= r:
-                if n % f == 0: return False
-                if n % (f+2) == 0: return False
-                f += 6
-            return True   
+        
+        maxNum = max(diag)
+        primes = {i for i in range(2, maxNum + 1)}
+        for i in range(2, maxNum + 1):
+            if i not in primes:
+                continue
+            for j in range(i * i, maxNum + 1, i):
+                if j in primes:
+                    primes.remove(j)
         
         maxPrime = 0
         for i in diag:
-            if isPrime(i):
+            if i in primes:
                 maxPrime = max(maxPrime, i)
         return maxPrime
