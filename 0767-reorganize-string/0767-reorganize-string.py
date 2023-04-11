5c70e45d3f1972c129b4d3d674da065fddde2ceb
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-04-11 11:43:40 +0530

    Time: 27 ms (96.39%), Space: 13.8 MB (97.39%) - LeetHub

--- 0767-reorganize-string/0767-reorganize-string.py
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -5,18 +5,17 @@ class Solution:
         for i in s:
             freqMap[indexOf(i)] += 1
         
-        letters = deque(sorted([[chr(i + ord('a')), freqMap[i]] for i in range(26)], key = lambda x: -x[1]))
+        letters = sorted([[chr(i + ord('a')), freqMap[i]] for i in range(26)], key = lambda x: -x[1])
         newS = [''] * len(s)
         i = 0
-        while letters:
-            for j in range(letters[0][1]):
+        for letter in letters:
+            for j in range(letter[1]):
                 if i >= len(s):
                     i %= len(s)
                 while i == len(s) or newS[i]:
                     i = (i + 1) % len(s)
-                newS[i] = letters[0][0]
+                newS[i] = letter[0]
                 i += 2
-            letters.popleft()
         for i in range(1, len(s)):
             if newS[i] == newS[i - 1]:
                 return ""
