8cbfad2ee1dd3d80f0f6acc8432f965c736988fa
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-04-15 21:43:34 +0530

    Time: 1118 ms (100.00%), Space: 17.4 MB (100.00%) - LeetHub

---
+++ 2642-design-graph-with-shortest-path-calculator/2642-design-graph-with-shortest-path-calculator.py
@@ -0,0 +1,31 @@
+class Graph:
+
+    def __init__(self, n: int, edges: List[List[int]]):
+        self.nodeMap = {i: set() for i in range(n)}
+        for i, j, w in edges:
+            self.nodeMap[i].add((j, w))
+        self.dists = [[float("inf")] * n for i in range(n)]
+        for i in range(n):
+            self.dists[i][i] = 0
+        
+    def addEdge(self, edge: List[int]) -> None:
+        self.nodeMap[edge[0]].add((edge[1], edge[2]))
+
+    def shortestPath(self, node1: int, node2: int) -> int:
+        h = [(0, node1)]
+        visited = set()
+        while h:
+            cost, node = heappop(h)
+            if node == node2:
+                return cost
+            if node in visited:
+                continue
+            visited.add(node)
+            for nei, weight in self.nodeMap[node]:
+                heappush(h, [cost + weight, nei])
+        return -1
+        
+# Your Graph object will be instantiated and called as such:
+# obj = Graph(n, edges)
+# obj.addEdge(edge)
+# param_2 = obj.shortestPath(node1,node2)
