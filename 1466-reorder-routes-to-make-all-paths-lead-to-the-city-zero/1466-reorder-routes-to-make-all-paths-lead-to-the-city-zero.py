3aed5df0f162a41f81064564517584c5ecc76cce
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-04-19 13:52:50 +0530

    Time: 1101 ms (94.69%), Space: 42.4 MB (82.26%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -1,17 +1,21 @@
 class Solution:
     def minReorder(self, n: int, connections: List[List[int]]) -> int:
-        dt = defaultdict(list)
-        for a,b in connections:
-            dt[a].append((b,True))
-            dt[b].append((a,False))
-        ans = 0
-        q = [0]
-        vis = set()
-        while(q):
-            node = q.pop()
-            vis.add(node)
-            for nd,st in dt[node]:
-                if nd not in vis:
-                    q.append(nd)
-                    if st: ans+=1
-        return ans
+        nodeMap = {i: [] for i in range(n)}
+        for i, j in connections:
+            nodeMap[i].append((j, True))
+            nodeMap[j].append((i, False))
+            
+        q = deque([0])
+        toReverse = 0
+        visited = {0}
+        while q:
+            city = q.popleft()
+            for nei, isForward in nodeMap[city]:
+                if nei in visited:
+                    continue
+                visited.add(nei)
+                q.append(nei)
+                if isForward:
+                    toReverse += 1
+        return toReverse
+        
