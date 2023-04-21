328444f008bcd51864ceed65a619cc3dd22f5794
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-04-21 12:33:44 +0530

    Time: 55 ms (23.69%), Space: 14.3 MB (29.77%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.py
@@ -1,16 +1,26 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        cost = 0
-        last7 = deque()
-        last30 = deque()
-        for day in days:
-            while last7 and last7[0][0] + 7 <= day:
-                last7.popleft()
-            while last30 and last30[0][0] + 30 <= day:
-                last30.popleft()
-            last7.append((day, cost + costs[1]))
-            last30.append((day, cost + costs[2]))
-            cost = min(cost + costs[0],
-                      last7[0][1],
-                      last30[0][1])
-        return cost
+        days.sort()
+        
+        @cache
+        def dp(index: int) -> int:
+            if index == len(days):
+                return 0
+            res = costs[0] + dp(index + 1)
+            for i in range(index + 1, index + 8):
+                if i == len(days):
+                    res = min(res, costs[1])
+                    break
+                if days[i] > days[index] + 6:
+                    res = min(res, costs[1] + dp(i))
+                    break
+            for i in range(index + 1, index + 31):
+                if i == len(days):
+                    res = min(res, costs[2])
+                    break
+                if days[i] > days[index] + 29:
+                    res = min(res, costs[2] + dp(i))
+                    break
+            return res
+        
+        return dp(0) 
