ad50330829e327facb32e0dac3beee93496038c9
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-04-23 10:34:49 +0530

    Time: 30 ms (98.26%), Space: 13.8 MB (54.40%) - LeetHub

--- 0179-largest-number/0179-largest-number.py
+++ 0179-largest-number/0179-largest-number.py
@@ -2,11 +2,11 @@ class Solution:
     def largestNumber(self, nums: List[int]) -> str:
         def compare(n1: str, n2: str) -> int:
             if n1 + n2 >= n2 + n1:
-                return 1
-            else:
                 return -1
+            else:
+                return 1
         
         nums = list(map(str, nums))
-        nums.sort(key = cmp_to_key(compare), reverse=True)
+        nums.sort(key = cmp_to_key(compare))
         # print(nums)
         return str(int(''.join(nums)))
