f1945be4c0ebc09b368983a6dfceff1f32f9c6b9
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-04-26 01:57:53 +0530

    Time: 5385 ms (5.00%), Space: 28.6 MB (22.80%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.py
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.py
@@ -1,15 +1,17 @@
 class Solution:
-    @cache
     def longestSubstring(self, s: str, k: int) -> int:
-        maxLen = 0
-        freqMap = [0] * 26
-        indexOf = lambda x: ord(x) - ord('a')
         
-        for i in s:
-            freqMap[indexOf(i)] += 1
+        def rec(l: int, r: int) -> int:
+            freqMap = [0] * 26
+            indexOf = lambda x: ord(x) - ord('a')
         
-        for i in range(len(s)):
-            if freqMap[indexOf(s[i])] < k:
-                freqMap[indexOf(s[i])] -= 1
-                return max(self.longestSubstring(s[:i], k), self.longestSubstring(s[i + 1:], k))
-        return len(s)
+            for i in range(l, r):
+                freqMap[indexOf(s[i])] += 1
+        
+            for i in range(l, r):
+                if freqMap[indexOf(s[i])] < k:
+                    freqMap[indexOf(s[i])] -= 1
+                    return max(rec(l, i), rec(i + 1, r))
+            return r - l
+        
+        return rec(0, len(s))
