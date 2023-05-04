bca2cc6e203e1897a58ba5db01257b142f3ba72a
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-05-04 11:09:16 +0530

    Time: 2542 ms (64.95%), Space: 357.6 MB (26.74%) - LeetHub

--- 0879-profitable-schemes/0879-profitable-schemes.py
+++ 0879-profitable-schemes/0879-profitable-schemes.py
@@ -6,37 +6,11 @@ class Solution:
         
         @cache
         def dp(index: int, n: int, score: int) -> int:
-            if index == len(crimes):
+            if index == len(crimes) or n < crimes[index][0]:
                 return 1 if score >= minProfit else 0
             res = dp(index + 1, n, score)
-            if n >= crimes[index][0]:
-                res += dp(index + 1, n - crimes[index][0], min(score + crimes[index][1], minProfit))
+            # if n >= crimes[index][0]:
+            res += dp(index + 1, n - crimes[index][0], min(score + crimes[index][1], minProfit))
             return res % mod
             
         return dp(0, n, 0)
-
-# class Solution:
-#     def profitableSchemes(self, n: int, minProfit: int, group: List[int], profit: List[int]) -> int:
-
-
-#         @cache
-#         def dfs(i, members, cur_profit):
-
-#             if i >= len(profit):
-#                 if cur_profit >= minProfit and members <= n:
-#                     return 1
-#                 else:
-#                     return 0
-                   
-                
-#             ans = 0
-#             ans += dfs(i + 1, members, cur_profit)
-            
-#             if members + group[i] <= n:
-
-#                 ans += dfs(i + 1, members + group[i], min(cur_profit + profit[i], minProfit))
-        
-#             return ans
-            
-#         return dfs(0, 0, 0) % (10**9 + 7)
-        
