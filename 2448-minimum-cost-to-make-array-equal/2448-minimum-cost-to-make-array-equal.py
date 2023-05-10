a83d4c508b5e273de6ace9a5fc3bcc50732e32ab
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-05-10 12:27:08 +0530

    Time: 408 ms (84.17%), Space: 34.5 MB (33.33%) - LeetHub

---
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.py
@@ -0,0 +1,27 @@
+class Solution:
+    def minCost(self, nums: List[int], cost: List[int]) -> int:
+        nums = [(nums[i], cost[i]) for i in range(len(nums))]
+        nums.sort()
+        n = sum(i[1] for i in nums)
+        
+        def calcCost(val: int) -> int:
+            cost = 0
+            for i, j in nums:
+                cost += abs(val - i) * j
+            return cost
+        
+        if n % 2:
+            currN = 0
+            for i, j in nums:
+                currN += j
+                if currN >= n // 2 + 1:
+                    return calcCost(i)
+        else:
+            currN = 0
+            res = float("inf")
+            for i, j in nums:
+                currN += j
+                if currN >= n // 2:
+                    res = min(res, calcCost(i))
+                if currN >= n // 2 + 1:
+                    return min(res, calcCost(i))
