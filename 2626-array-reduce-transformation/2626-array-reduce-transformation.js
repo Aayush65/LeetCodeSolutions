c39d8905cd39411cc9d60248d3ee8050ef3ca418
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-05-10 12:34:50 +0530

    Time: 58 ms (74.71%), Space: 42 MB (88.95%) - LeetHub

---
+++ 2626-array-reduce-transformation/2626-array-reduce-transformation.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+var reduce = function(nums, fn, init) {
+    let accum = init;
+    for (let i= 0; i < nums.length; i ++) 
+        accum = fn(accum, nums[i])
+    return accum;
+};
