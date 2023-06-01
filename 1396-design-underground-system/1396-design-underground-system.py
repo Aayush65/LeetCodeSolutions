07409b524103218a7061469837f7627db745d1a7
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-06-01 21:37:40 +0530

    Time: 251 ms (50.87%), Space: 27.4 MB (22.58%) - LeetHub

--- 1396-design-underground-system/1396-design-underground-system.py
+++ 1396-design-underground-system/1396-design-underground-system.py
@@ -1,24 +1,24 @@
 class UndergroundSystem:
 
     def __init__(self):
-        self.journey = {}
-        self.history = {} # (startStation, endStation) => (allTime, allCount)
+        self.checkinTime = {}
+        self.avgMap = {}
 
-    def checkIn(self, id: int, startStation: str, t: int) -> None:
-        self.journey[id] = (startStation, t)
-        
+    def checkIn(self, id: int, stationName: str, t: int) -> None:
+        self.checkinTime[id] = (stationName, t)
 
-    def checkOut(self, id: int, endStation: str, endTime: int) -> None:
-        startStation, startTime = self.journey.pop(id)
-        key = (startStation, endStation)
-        allTime, allCount = self.history.get(key, (0, 0))
-        self.history[key] = (allTime + (endTime - startTime), allCount + 1)
-        
+    def checkOut(self, id: int, stationName: str, t: int) -> None:
+        start, startTime = self.checkinTime[id]
+        timeTaken = t - startTime
+        if (start, stationName) in self.avgMap:
+            self.avgMap[(start, stationName)] = (self.avgMap[(start, stationName)][0] + timeTaken, self.avgMap[(start, stationName)][1] + 1)
+        else:
+            self.avgMap[(start, stationName)] = (timeTaken, 1)
 
     def getAverageTime(self, startStation: str, endStation: str) -> float:
-        key = (startStation, endStation)
-        allTime, allCount = self.history.get(key, (0, 0))
-        return allTime / allCount
+        time, person = self.avgMap[(startStation, endStation)]
+        return time / person
+
 
 # Your UndergroundSystem object will be instantiated and called as such:
 # obj = UndergroundSystem()
