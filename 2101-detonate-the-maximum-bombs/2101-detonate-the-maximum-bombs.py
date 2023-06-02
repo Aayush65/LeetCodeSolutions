1fbcea0c02ae493176873c473587fa7f1ed1eea4
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-06-02 21:35:56 +0530

    Time: 1204 ms (28.89%), Space: 16.7 MB (33.91%) - LeetHub

--- 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.py
@@ -4,22 +4,24 @@ class Solution:
         # Union find only works on non directed graphs, but doesn't work on directed graphs
 
         n = len(bombs)
-        dist = lambda c1, c2: (c1[0] - c2[0]) ** 2 + (c1[1] - c2[1]) ** 2 
+        dist = lambda c1, c2: (c1[0] - c2[0]) ** 2 + (c1[1] - c2[1]) ** 2
+
+        nodeMap = {i: [] for i in range(n)}
+        for i in range(n):
+            for j in range(n):
+                if i == j: continue
+                if dist(bombs[i][:2], bombs[j][:2]) <= bombs[i][2] ** 2:
+                    nodeMap[i].append(j)
 
         memo = {}
-        def dfs(index: int) -> int:
-            if (index, tuple(visited)) in memo:
-                return memo[(index, tuple(visited))]
-            x1, y1, r1 = bombs[index]
-            visited[index] = 1
+        def dfs(node: int) -> int:
+            if (node, tuple(visited)) in memo:
+                return memo[(node, tuple(visited))]
+            visited[node] = 1
             blastZone = 1
-            for i in range(n):
-                x2, y2, r2 = bombs[i]
-                if visited[i]:
-                    continue
-                if dist((x1, y1), (x2, y2)) <= r1 ** 2:
-                    blastZone += dfs(i)
-            memo[(index, tuple(visited))] = blastZone
+            for nei in nodeMap[node]:
+                if visited[nei]: continue
+                blastZone += dfs(nei)
             return blastZone
 
         maxBlastZone = 1
