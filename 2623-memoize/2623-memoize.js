1465bf7ee229254f7d873939d6a2b6e0279823e7
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-06-04 00:28:44 +0530

    Time: 342 ms (70.10%), Space: 107.2 MB (54.21%) - LeetHub

---
+++ 2623-memoize/2623-memoize.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Function} fn
+ */
+function memoize(fn) {
+    const memo = {}
+    return function(...args) {
+        const key = JSON.stringify(args)
+        if (key in memo)
+            return memo[key]
+        memo[key] = fn(...args)
+        return memo[key]
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
