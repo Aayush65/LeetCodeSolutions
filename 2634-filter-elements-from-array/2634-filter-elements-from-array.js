87b82d9b7627309aabd34dde041d251bacb87061
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-06-03 15:57:37 +0530

    Time: 66 ms (15.17%), Space: 42.1 MB (43.34%) - LeetHub

--- 2634-filter-elements-from-array/2634-filter-elements-from-array.js
+++ 2634-filter-elements-from-array/2634-filter-elements-from-array.js
@@ -4,6 +4,9 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    const res = arr.filter(fn)
+    const res = []
+    for (const i in arr)
+        if (fn(arr[i], Number(i)))
+            res.push(arr[i])
     return res
 };
