c76f81e180b9348c3cae54bc48464b5d3b274036
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-06-21 11:12:47 +0530

    Time: 430 ms (88.24%), Space: 34.5 MB (45.59%) - LeetHub

--- 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.py
+++ 2448-minimum-cost-to-make-array-equal/2448-minimum-cost-to-make-array-equal.py
@@ -10,18 +10,18 @@ class Solution:
                 cost += abs(val - i) * j
             return cost
         
-        if n % 2:
-            currN = 0
-            for i, j in nums:
-                currN += j
-                if currN > n // 2:
-                    return calcCost(i)
-        else:
-            currN = 0
-            res = float("inf")
-            for i, j in nums:
-                currN += j
-                if currN >= n // 2:
-                    res = min(res, calcCost(i))
-                if currN > n // 2:
-                    return min(res, calcCost(i))
+        # if n % 2:
+        #     currN = 0
+        #     for i, j in nums:
+        #         currN += j
+        #         if currN > n // 2:
+        #             return calcCost(i)
+        # else:
+        currN = 0
+        res = float("inf")
+        for i, j in nums:
+            currN += j
+            if currN >= n // 2:
+                res = min(res, calcCost(i))
+            if currN > n // 2:
+                return min(res, calcCost(i))
