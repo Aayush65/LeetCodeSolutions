fcfe2c56c60f7742e2ed72574ea688ac58764589
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-07-23 09:59:00 +0530

    Time: 9992 ms (14.29%), Space: 31.4 MB (14.29%) - LeetHub

---
+++ 2790-maximum-number-of-groups-with-increasing-length/2790-maximum-number-of-groups-with-increasing-length.py
@@ -0,0 +1,30 @@
+class Solution:
+    def maxIncreasingGroups(self, usageLimits: List[int]) -> int:
+        usageLimits.sort(reverse=True)
+        n = len(usageLimits)
+        
+        def check(val: int) -> int:
+            h = [-val]
+            for i in usageLimits:
+                tempI = i
+                while tempI > 0 and h:
+                    largest = -heappop(h)
+                    largest, tempI = largest - tempI, tempI - largest
+                    if largest > 0:
+                        heappush(h, -largest)
+                val -= 1
+                if val <= 0 and not h:
+                    break
+                if val > 0:
+                    heappush(h, -val)
+            return len(h) == 0 and val <= 0
+            
+            
+        lo, hi = 0, min(sum(usageLimits), (n * (n + 1)) // 2)
+        while lo < hi:
+            mid = (lo + hi + 1) // 2
+            if check(mid):
+                lo = mid
+            else:
+                hi = mid - 1
+        return lo
