bcec75fe83e10b0f2bcac448d046e2775aaf7c98
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-07-23 21:58:49 +0530

    Time: 840 ms (100.00%), Space: 30.3 MB (100.00%) - LeetHub

--- 2790-maximum-number-of-groups-with-increasing-length/2790-maximum-number-of-groups-with-increasing-length.py
+++ 2790-maximum-number-of-groups-with-increasing-length/2790-maximum-number-of-groups-with-increasing-length.py
@@ -1,30 +1,41 @@
 class Solution:
     def maxIncreasingGroups(self, usageLimits: List[int]) -> int:
-        usageLimits.sort(reverse=True)
+        # usageLimits.sort(reverse=True)
         n = len(usageLimits)
         
-        def check(val: int) -> int:
-            h = [-val]
-            for i in usageLimits:
-                tempI = i
-                while tempI > 0 and h:
-                    largest = -heappop(h)
-                    largest, tempI = largest - tempI, tempI - largest
-                    if largest > 0:
-                        heappush(h, -largest)
-                val -= 1
-                if val <= 0 and not h:
-                    break
-                if val > 0:
-                    heappush(h, -val)
-            return len(h) == 0 and val <= 0
+#         def check(val: int) -> int:
+#             h = [-val]
+#             for i in usageLimits:
+#                 tempI = i
+#                 while tempI > 0 and h:
+#                     largest = -heappop(h)
+#                     largest, tempI = largest - tempI, tempI - largest
+#                     if largest > 0:
+#                         heappush(h, -largest)
+#                 val -= 1
+#                 if val <= 0 and not h:
+#                     break
+#                 if val > 0:
+#                     heappush(h, -val)
+#             return len(h) == 0 and val <= 0
             
             
-        lo, hi = 0, min(sum(usageLimits), (n * (n + 1)) // 2)
-        while lo < hi:
-            mid = (lo + hi + 1) // 2
-            if check(mid):
-                lo = mid
-            else:
-                hi = mid - 1
-        return lo
+#         lo, hi = 0, min(sum(usageLimits), (n * (n + 1)) // 2)
+#         while lo < hi:
+#             mid = (lo + hi + 1) // 2
+#             if check(mid):
+#                 lo = mid
+#             else:
+#                 hi = mid - 1
+#         return lo
+
+        usageLimits.sort()
+        count = 0
+        total = 0
+        for i in range(n):
+            total += usageLimits[i]
+            nextReq = count + 1
+            if total >= nextReq:
+                total -= nextReq
+                count += 1
+        return count
