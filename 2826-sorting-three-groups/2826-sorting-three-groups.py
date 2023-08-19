ed7a2c8fa915e80e21143d4e90dbb37fa722b68f
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-08-20 00:24:48 +0530

    Time: 244 ms (100.00%), Space: 17 MB (40.00%) - LeetHub

--- 2826-sorting-three-groups/2826-sorting-three-groups.py
+++ 2826-sorting-three-groups/2826-sorting-three-groups.py
@@ -1,19 +1,13 @@
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
-        
-        memo = [[-1] * 3  for i in range(len(nums))]
-        
-        def dp(index: int, mini: int) -> int:
+
+        @cache
+        def dp(index: int, prev: int) -> int:
             if index == len(nums):
                 return 0
-            if memo[index][mini - 1] != -1:
-                return memo[index][mini - 1]
-            if nums[index] == mini:
-                return dp(index + 1, mini)
-            res = 1 + dp(index + 1, mini)
-            if nums[index] > mini:
-                res = min(res, dp(index + 1, nums[index]))
-            memo[index][mini - 1] = res
-            return res            
+            res = dp(index + 1, prev)
+            if nums[index] >= prev:
+                res = max(res, 1 + dp(index + 1, nums[index]))
+            return res
         
-        return dp(0, 1)
+        return len(nums) - dp(0, 1)
