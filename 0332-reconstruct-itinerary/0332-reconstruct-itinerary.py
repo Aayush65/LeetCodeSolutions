6022e56e3e8391eb07942bb096cd470ce56da0d0
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-09-14 22:21:14 +0530

    Time: 123 ms (8.49%), Space: 17.2 MB (44.26%) - LeetHub

---
+++ 0332-reconstruct-itinerary/0332-reconstruct-itinerary.py
@@ -0,0 +1,30 @@
+class Solution:
+    def findItinerary(self, tickets: List[List[str]]) -> List[str]:
+        from sortedcontainers import SortedList
+        
+        destMap = {i[0]: SortedList() for i in tickets}
+        for i, j in tickets:
+            destMap[i].add(j)
+        
+        itenerary = ['zzzz']
+        
+        def dfs(city: str, path: list[str]) -> bool:
+            if city not in destMap or not destMap[city]:
+                if len(path) != len(tickets) + 1:
+                    return False
+                nonlocal itenerary
+                itenerary = min(itenerary, path)
+                return True
+
+            for i in range(len(destMap[city])):
+                nextCity = destMap[city][i]
+                destMap[city].pop(i)
+                path.append(nextCity)
+                if dfs(nextCity, path):
+                    return True
+                path.pop()
+                destMap[city].add(nextCity)
+            return False
+
+        dfs("JFK", ["JFK"])
+        return itenerary
