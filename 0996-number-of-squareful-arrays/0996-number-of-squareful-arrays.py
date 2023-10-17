bed1a8716e43915d2304395551e8e9f45cfdbe0f
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-10-18 00:05:04 +0530

    Time: 34 ms (95.67%), Space: 16.3 MB (74.04%) - LeetHub

--- 0996-number-of-squareful-arrays/0996-number-of-squareful-arrays.py
+++ 0996-number-of-squareful-arrays/0996-number-of-squareful-arrays.py
@@ -1,11 +1,7 @@
 class Solution:
     def numSquarefulPerms(self, nums: List[int]) -> int:
-        nums.sort()        
         
-        memo = {}
         def dp(num: int, nums: list[int]) -> int:
-            if (num, tuple(nums)) in memo:
-                return memo[(num, tuple(nums))]
             nums.remove(num)
             if not nums:
                 return 1
@@ -14,8 +10,6 @@ class Solution:
                 sqrt = (i + num) ** 0.5
                 if sqrt == int(sqrt):
                     res += dp(i, nums.copy())
-                                
-            # memo[(num, tuple(nums))] = res
             return res
             
         return sum(dp(i, nums.copy()) for i in set(nums))
