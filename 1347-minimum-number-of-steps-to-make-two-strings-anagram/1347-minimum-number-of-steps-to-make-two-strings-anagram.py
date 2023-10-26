2457fe96c70863c71264a5afd39595766fec1c43
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-10-26 23:57:58 +0530

    Time: 262 ms (7.26%), Space: 16.8 MB (69.77%) - LeetHub

---
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minSteps(self, s: str, t: str) -> int:
+        indexOf = lambda x: ord(x) - ord('a')
+        
+        freq1 = [0] * 26
+        freq2 = [0] * 26
+        for i in s:
+            freq1[indexOf(i)] += 1
+        for i in t:
+            freq2[indexOf(i)] += 1
+        
+        res = 0
+        for i, j in zip(freq1, freq2):
+            if not i and not j:
+                continue
+            if i - j > 0:
+                res += i - j
+        return res
