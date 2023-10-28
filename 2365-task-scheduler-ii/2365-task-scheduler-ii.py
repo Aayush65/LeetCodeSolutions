1e5dd403ada9ea51f97d9d9fae53a21c841bda94
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-10-29 03:52:26 +0530

    Time: 771 ms (66.49%), Space: 33.7 MB (74.48%) - LeetHub

---
+++ 2365-task-scheduler-ii/2365-task-scheduler-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def taskSchedulerII(self, tasks: List[int], space: int) -> int:
+        lastDate = {i: -space for i in tasks}
+        day = 1
+        for i in tasks:
+            if day - lastDate[i] >= space:
+                day += 1
+            else:
+                day = space + lastDate[i] + 1
+            lastDate[i] = day
+        return day - 1
