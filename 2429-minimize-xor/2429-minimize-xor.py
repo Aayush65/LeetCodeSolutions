b75ae2af39699b38cb7aaf4e73b07fabefcf5fcc
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-11-13 02:31:41 +0530

    Time: 39 ms (56.91%), Space: 16.1 MB (71.54%) - LeetHub

---
+++ 2429-minimize-xor/2429-minimize-xor.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minimizeXor(self, num1: int, num2: int) -> int:
+        bits = bin(num2).count('1')
+        zeroes = []
+        res = num1
+        for i in range(32, -1, -1):
+            if num1 & (1 << i):
+                res ^= 1 << i
+                bits -= 1
+            else:
+                zeroes.append(i)
+            if not bits:
+                break
+        
+        for i in range(bits):
+            res |= 1 << zeroes.pop()
+        return res ^ num1
