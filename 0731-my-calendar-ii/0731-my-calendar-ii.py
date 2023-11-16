fe1ab22c558fc79072c255e8fb4028706a415489
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-11-17 01:05:51 +0530

    Time: 2374 ms (11.46%), Space: 17.4 MB (17.88%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.py
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -1,41 +1,60 @@
 class MyCalendarTwo:
 
     def __init__(self):
-        from sortedcontainers import SortedList
-        self.time = SortedList([], key = lambda x: x[0])
+        self.time = defaultdict(int)
 
     def book(self, start: int, end: int) -> bool:
-        sIdx = self.time.bisect_left([start])
-        if sIdx < len(self.time) and self.time[sIdx][0] == start:
-            self.time[sIdx][1] += 1
-        else:
-            self.time.add([start, 1])
-        
-        eIdx = self.time.bisect_left([end])
-        if eIdx < len(self.time) and self.time[eIdx][0] == end:
-            self.time[eIdx][1] -= 1
-        else:
-            self.time.add([end, -1])
+        self.time[start] += 1
+        self.time[end] -= 1
         
         total = 0
-        toAdd = True
-        for i in range(eIdx + 1):
-            total += self.time[i][1]
+        for i in sorted(self.time.keys()):
+            total += self.time[i]
             if total == 3:
-                toAdd = False
-                break
+                self.time[start] -= 1
+                self.time[end] += 1
+                return False
+        return True
+
+    
+    
+#     def __init__(self):
+#         from sortedcontainers import SortedList
+#         self.time = SortedList([], key = lambda x: x[0])
+
+
+#     def book(self, start: int, end: int) -> bool:
+#         sIdx = self.time.bisect_left([start])
+#         if sIdx < len(self.time) and self.time[sIdx][0] == start:
+#             self.time[sIdx][1] += 1
+#         else:
+#             self.time.add([start, 1])
         
-        if toAdd:
-            return True
+#         eIdx = self.time.bisect_left([end])
+#         if eIdx < len(self.time) and self.time[eIdx][0] == end:
+#             self.time[eIdx][1] -= 1
+#         else:
+#             self.time.add([end, -1])
         
-        self.time[sIdx][1] -= 1
-        if not self.time[sIdx][1]:
-            eIdx -= 1
-            self.time.pop(sIdx)
-        self.time[eIdx][1] += 1
-        if not self.time[eIdx][1]:
-            self.time.pop(eIdx)
-        return False
+#         total = 0
+#         toAdd = True
+#         for i in range(eIdx + 1):
+#             total += self.time[i][1]
+#             if total == 3:
+#                 toAdd = False
+#                 break
+        
+#         if toAdd:
+#             return True
+        
+#         self.time[sIdx][1] -= 1
+#         if not self.time[sIdx][1]:
+#             eIdx -= 1
+#             self.time.pop(sIdx)
+#         self.time[eIdx][1] += 1
+#         if not self.time[eIdx][1]:
+#             self.time.pop(eIdx)
+#         return False
         
     
 # Your MyCalendarTwo object will be instantiated and called as such:
