4d7eb35d76696960f4072be17504ef48b9cb2afc
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-11-18 18:27:55 +0530

    Time: 1792 ms (56.90%), Space: 34.8 MB (42.19%) - LeetHub

--- 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
+++ 0307-range-sum-query-mutable/0307-range-sum-query-mutable.py
@@ -1,42 +1,80 @@
 class SegmentTree:
-    def __init__(self, nums: list[int]):
-        n = len(nums) + 1
-        self.nums = [0] * (n - 1)
-        self.rangeVals = [0] * n
-        for i in range(n - 1):
-            self.update(i, nums[i])
-        
-        
-    def update(self, index: int, val: int) -> None:
-        diff = val - self.nums[index]
-        self.nums[index] = val
-        index += 1
-        while index < len(self.rangeVals):
-            self.rangeVals[index] += diff
-            index += index & -index
-    
-    def rangeQuery(self, left: int, right: int) -> int:
-        total = 0
-        right += 1
-        while right > 0:
-            total += self.rangeVals[right]
-            right -= right & -right
-        while left > 0:
-            total -= self.rangeVals[left]
-            left -= left & -left
-        return total
-        
 
+    def __init__(self, nums: List[int], identity_val: Optional[int], cmp: Callable, key: Callable = lambda x: x):
+        self.n = len(nums)
+        self.identity_val = identity_val
+        self.key = key
+        self.cmp = cmp
+
+        self.nums = nums
+        self.__build_nums()
+        self.tree = [identity_val] * self.n * 2
+        self.__build_tree()
+
+    # Prints the segment tree when the Object is printed
+    def __str__(self) -> str:
+        return self.tree.__str__()
+    
+    # This function builds the main array at initial initialisation using key function
+    def __build_nums(self):
+        self.nums = [self.key(x) for x in self.nums]
+
+    # This function builds the segment tree at initial initialisation
+    def __build_tree(self):
+        for i in range(self.n):
+            self.tree[i + self.n] = self.nums[i]
+        for i in range(self.n - 1, 0, -1):
+            self.tree[i] = self.cmp(self.tree[2 * i], self.tree[2 * i + 1])
+
+    # This function updates the values at the index to the new value
+    def update(self, index: int, val: int):
+        index += self.n
+        self.tree[index] = val
+        while index > 1:
+            new_val = self.cmp(self.tree[index], self.tree[index ^ 1])
+            index //= 2
+            if self.tree[index] == new_val:
+                break
+            self.tree[index] = new_val
+
+    # This function removes all the instances of the val from the tree
+    def remove(self, val: int) -> int:
+        removed = 0
+        for i in range(self.n):
+            if self.tree[self.n + i] == val:
+                self.update(i, self.identity_val)
+                removed += 1
+        return removed
+
+    # This function returns the result between the left and right indices (not inclusive)
+    def query(self, left: int, right: int) -> int:
+        res = self.identity_val
+        left += self.n
+        right += self.n
+        # Uncomment the below to change this function to inclusive left and right pointers
+        right += 1 
+        while left < right:
+            if left & 1:
+                res = self.cmp(res, self.tree[left])
+                left += 1
+            if right & 1:
+                right -= 1
+                res = self.cmp(res, self.tree[right])
+            left //= 2
+            right //= 2
+        return res
+        
+        
 class NumArray:
 
     def __init__(self, nums: list[int]):
-        self.st = SegmentTree(nums)
+        self.st = SegmentTree(nums, identity_val= 0, cmp = lambda x1, x2: x1 + x2)
 
     def update(self, index: int, val: int) -> None:
         self.st.update(index, val)
 
     def sumRange(self, left: int, right: int) -> int:
-        return self.st.rangeQuery(left, right)
+        return self.st.query(left, right)
 
 # Your NumArray object will be instantiated and called as such:
 # obj = NumArray(nums)
