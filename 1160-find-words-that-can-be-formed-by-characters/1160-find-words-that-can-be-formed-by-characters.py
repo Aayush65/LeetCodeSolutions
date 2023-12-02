3e85b19aac4c4754dc05c4cd26d77056d50e8bdd
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-12-02 14:19:01 +0530

    Time: 155 ms (55.05%), Space: 16.9 MB (13.86%) - LeetHub

--- 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
+++ 1160-find-words-that-can-be-formed-by-characters/1160-find-words-that-can-be-formed-by-characters.py
@@ -5,7 +5,7 @@ class Solution:
         for word in words:
             freq = Counter(word)
             isGood = True
-            for i in word:
+            for i in freq:
                 if i not in charFreq or charFreq[i] < freq[i]:
                     isGood = False
                     break
