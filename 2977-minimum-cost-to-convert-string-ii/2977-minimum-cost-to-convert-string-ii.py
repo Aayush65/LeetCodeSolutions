89b68437ebb0ca2adde779f34a575e7746dfefeb
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-12-29 01:26:34 +0530

    Time: 1973 ms (61.93%), Space: 30.9 MB (19.10%) - LeetHub

--- 2977-minimum-cost-to-convert-string-ii/2977-minimum-cost-to-convert-string-ii.py
+++ 2977-minimum-cost-to-convert-string-ii/2977-minimum-cost-to-convert-string-ii.py
@@ -1,22 +1,25 @@
 class Solution:
     def minimumCost(self, source: str, target: str, original: List[str], changed: List[str], cost: List[int]) -> int:
-        allPoints = {x: i for i, x in enumerate(list(set(original + changed)))}
-        n = len(allPoints)
-        dist = [[float("inf")] * n for _ in range(n)]
-        for i in range(n):
-            dist[i][i] = 0
-
+        strMap = defaultdict(list)
         for i, j, val in zip(original, changed, cost):
-            i, j = allPoints[i], allPoints[j]
-            dist[i][j] = min(dist[i][j], val)
-
-        for k in range(n):
-            for i in range(n):
-                for j in range(n):
-                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
-                    
-        allLens = sorted(list({len(i) for i in allPoints}))
+            strMap[i].append((val, j))
+        allLens = sorted(list({len(i) for i in original}))
         n = len(source)
+        
+        @cache
+        def shortestDist(start: str, end: str) -> int:
+            h = [(0, start)]
+            visited = set()
+            while h:
+                cost, curr = heappop(h)
+                if curr == end:
+                    return cost
+                if curr in visited:
+                    continue
+                visited.add(curr)
+                for nextCost, nei in strMap[curr]:
+                    heappush(h, (cost + nextCost, nei))
+            return float("inf")
         
         @cache
         def dp(index: int) -> int:
@@ -30,10 +33,9 @@ class Solution:
                     break
                 srcSubStr = source[index: index + i]
                 trgSubStr = target[index: index + i]
-                if srcSubStr in allPoints and trgSubStr in allPoints:
-                    cost = dist[allPoints[srcSubStr]][allPoints[trgSubStr]]
-                    if cost != float("inf"):
-                        res = min(res, cost + dp(index + i))
+                cost = shortestDist(srcSubStr, trgSubStr)
+                if cost != float("inf"):
+                    res = min(res, cost + dp(index + i))
             return res
 
         res = dp(0)
