b4635b29fb88604f1d96f8a9b78dde5fdf82c61b
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2023-12-29 01:06:20 +0530

    Time: 18295 ms (7.59%), Space: 21.8 MB (39.85%) - LeetHub

--- 2977-minimum-cost-to-convert-string-ii/2977-minimum-cost-to-convert-string-ii.py
+++ 2977-minimum-cost-to-convert-string-ii/2977-minimum-cost-to-convert-string-ii.py
@@ -14,24 +14,20 @@ class Solution:
                 adj[start][end] = c
         
         
-        @cache
-        def dijkstra(start, end):
-            heap = [(0, start)]
-            costs = defaultdict(lambda: inf)
-            costs[start] = 0
-            while heap:
-                path_cost, curr = heapq.heappop(heap)
-                if curr == end:
-                    return path_cost
-                for nei in adj[curr]:
-                    nei_cost = adj[curr][nei]
-                    
-                    new_cost = nei_cost + path_cost
-                    
-                    if new_cost < costs[nei]:
-                        costs[nei] = new_cost
-                        heapq.heappush(heap, (new_cost, nei))
-            return inf
+        allPoints = {x: i for i, x in enumerate(list(set(original + changed)))}
+        n = len(allPoints)
+        dist = [[float("inf")] * n for _ in range(n)]
+        for i in range(n):
+            dist[i][i] = 0
+
+        for i, j, val in zip(original, changed, cost):
+            i, j = allPoints[i], allPoints[j]
+            dist[i][j] = min(dist[i][j], val)
+
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
         
 
         @cache
@@ -39,12 +35,14 @@ class Solution:
             #let dfs(i) be the cost of matching everything at i and onwards assuming everything before i is matched
             if i >= len(target):
                 return 0
-
+            
             c = inf if target[i] != source[i] else dfs(i+1) #if they match save default cost as just continue
             for length in change_lengths:
                 t_sub = target[i:i+length]
                 s_sub = source[i:i+length]
-                trans_cost = dijkstra(s_sub, t_sub)
+                trans_cost = inf
+                if s_sub in allPoints and t_sub in allPoints:
+                    trans_cost = dist[allPoints[s_sub]][allPoints[t_sub]]
 
                 if trans_cost != inf:
                     c = min(c, trans_cost + dfs(i+length))
