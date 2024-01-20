d330831da244d5fa1c537a512fc4d42a75cbda0f
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2024-01-21 01:08:36 +0530

    Time: 3168 ms (7.79%), Space: 16.5 MB (66.55%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -1,3 +1,15 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
-        return "".join(list(itertools.permutations([str(i + 1) for i in range(n)], n))[k - 1])
+        
+        curr = [i + 1 for i in range(n)]
+        for _ in range(k - 1):
+            for i in range(n - 1, 0, -1):
+                if curr[i - 1] < curr[i]:
+                    break
+            for j in range(n - 1, i - 1, -1):
+                if curr[i - 1] < curr[j]:
+                    curr[i - 1], curr[j] = curr[j], curr[i - 1]
+                    break
+            curr = curr[:i] + curr[i:][::-1]
+            
+        return "".join(str(i) for i in curr)
