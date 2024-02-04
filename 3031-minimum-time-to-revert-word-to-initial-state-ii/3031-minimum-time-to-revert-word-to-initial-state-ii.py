b2d3960616df7cdef8939dffa7ed324e25349030
Aayush Gupta <79572409+Aayush65@users.noreply.github.com>
2024-02-04 09:44:32 +0530

    Time: 812 ms (33.33%), Space: 17.8 MB (100.00%) - LeetHub

--- 3031-minimum-time-to-revert-word-to-initial-state-ii/3031-minimum-time-to-revert-word-to-initial-state-ii.py
+++ 3031-minimum-time-to-revert-word-to-initial-state-ii/3031-minimum-time-to-revert-word-to-initial-state-ii.py
@@ -1,31 +1,26 @@
 class Solution:
     def minimumTimeToInitialState(self, word: str, k: int) -> int:
-        p = int(1e9 + 7)
-        indexOf = lambda x: ord(x) - ord('a')
+#         p = int(26639628671867)
+#         indexOf = lambda x: ord(x) - ord('a')
         
-        def check(idx):
-            j = len(word) - 1
-            for i in range(idx, -1, -1):
-                if word[i] != word[j]:
-                    return False
-                j -= 1
-            return True
+#         hash1 = hash2 = 0
+#         arr = []
+#         currPow = 1
         
+#         for i in range(len(word)):
+#             x, y = word[i], word[-i - 1]
+#             hash1 = (hash1 * 26 + indexOf(x)) % p
+#             hash2 = (indexOf(y) * currPow + hash2) % p
+#             arr.append(hash1 == hash2)
+#             currPow = (currPow * 26) % p
         
-        hash1 = hash2 = 0
-        arr = []
-        currPow = 1
-        
-        for i in range(len(word)):
-            x, y = word[i], word[-i - 1]
-            hash1 = (hash1 * 26 + indexOf(x)) % p
-            hash2 = (indexOf(y) * currPow + hash2) % p
-            arr.append(hash1 == hash2)
-            currPow = (currPow * 26) % p
-        
-        arr.reverse()
+#         arr.reverse()
+#         for i in range(k, len(word), k):
+#             if arr[i]:  return i // k
+            
+#         return ceil(len(word) / k)
+            
         for i in range(k, len(word), k):
-            if arr[i]:  return i // k
-            
+            if word.startswith(word[i:]):
+                return i // k
         return ceil(len(word) / k)
-            
